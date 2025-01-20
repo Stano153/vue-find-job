90449a01f515a431008cbc0e3f42961cfbf454e9
Stanislav Ondrej <stanislav.ondrej@student.tuke.sk>
2025-01-20 16:25:56 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import Hero from '@/components/Hero.vue';
+  import HomeCards from '@/components/Homecards.vue';
+  import JobListings from '@/components/JobListings.vue';
+</script>
+
+<template>
+  <Hero subtitle="Created by Stano"/>
+  <HomeCards />
+  <JobListings :limit="3" :show-button="true"/>
+</template>
